2ce4c7e35a7d33a42f3c1fa13cfa193fb723a8d5
oteeb <oteeb44@gmail.com>
2022-04-29 13:06:43 +0700

    ลบ

--- app.js
+++
@@ -1,6 +0,0 @@
-let http = require('http');
-
-http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type' : 'text/html'});
-    res.end('555');
-}).listen(3000, () => console.log("Server is successfully!"));
